2a7980b77a1f732db312b0a1a94f68f25df8bb90
Isaac Gorman <emailme@isaacg.net>
2020-08-05 08:01:06 -0500

    up and running

---
+++ study-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h6>Composing and Sharing Non-Visual Behavior</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
